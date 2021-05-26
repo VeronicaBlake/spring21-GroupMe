07de15fac923ccd6f0fab8c7ee4a47a1da989fa1
Veronica Blake/Noyce <vrex149@gmail.com>
2021-05-26 15:05:31 -0600

    changed env

--- GroupMe.Client/src/env.js
+++ GroupMe.Client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'https://localhost:5001' : ''
-export const domain = 'codeworksclassroom.auth0.com'
-export const audience = 'https://codeworksclassroom.com'
-export const clientId = 'pOXw2OGv1LsYi7LEBmDF04RLkXQvldml'
+export const domain = 'dev-k8azdd8c.us.auth0.com'
+export const audience = 'https://bugger.com'
+export const clientId = 'jgwvCI4WY9O6hnloTQMGOjatnfT2hCwe'
